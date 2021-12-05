f4ce855c0e8cdb54ae7eb48bdf691352078b52e4
Harold Groenenboom <harold_groe@hotmail.com>
2021-12-05 20:05:43 +0100

    Ugly software timer using arduino-timer (https://www.arduino.cc/reference/en/libraries/arduino-timer/)

--- tone.ino
+++ tone.ino
@@ -1,6 +1,8 @@
 #include "include/conversion.h"
 #include "include/midinote.h"
 
+#include <arduino-timer.h>
+
 const MidiNote notes[]
 {
   { 24, 0.0625f, false },
@@ -39,7 +41,34 @@ void loopNotes(int numTimes, float speedMultiplier)
   }
 }
 
-const int samplesPerBlock = 1024; 
+static const int sampleRate = 1000;
+Timer<50, micros> audioTimer;
+
+float phaseIncrement(float frequency)
+{
+  return static_cast<float>(sampleRate) / frequency;
+}
+
+float phase = 0.0f;
+inline float processAudio(float dither)
+{
+  static const auto ditherAmount = 0.05f;
+
+  phase += phaseIncrement(100.0f);
+  
+  return sin(phase) + ditherAmount * dither;
+}
+
+bool processTimer(void*)
+{
+  const float dither = -0.5f + 0.001f * static_cast<float>(random(1000));
+  
+  const float output = processAudio(dither);
+  
+  digitalWrite(8, (output > 0.0) ? HIGH : LOW);
+    
+  return true;
+}
 
 void setup() 
 {
@@ -48,9 +77,12 @@ void setup()
 
   constexpr float speedMultiplier =  multiplierFromBpm(120.0f);
 
-  loopNotes(10, speedMultiplier);
+  pinMode(8, OUTPUT);
+
+  audioTimer.every(1000000 / sampleRate, processTimer);
 }
 
 void loop() 
 {
+  audioTimer.tick();
 }
