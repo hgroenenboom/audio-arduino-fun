97f32ca59148f0d8b36df41087874afb58cbd09f
Harold Groenenboom <harold_groe@hotmail.com>
2021-12-05 21:03:22 +0100

    Added actual low level audio processing

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ Sources:
 Arduino uses its own GCC compiler version called `avr-gcc`. Arduino has very limited RAM, so there is no STD in avr-gcc. There are however libraries which add their own implementation of the STD for `arv-gcc`, but these are not official and provide varying results.
 
 ### Essentials
+Foundations: https://www.arduino.cc/en/Tutorial/Foundations
+
 - `Serial.begin(rate)`
     initiating the serial output with a given rate, this rate should match in Arduino IDEs serial monitor
 - `Serial.print()` and `Serial.println()`
@@ -26,3 +28,10 @@ Arduino uses its own GCC compiler version called `avr-gcc`. Arduino has very lim
 - bad performance: using a software timer library
 - high performance: using interupts and a hardware timer
 - using audiofile player libraries
+
+### PortManipulation 
+Sources:
+- https://electronoobs.com/eng_arduino_tut130.php
+- https://www.arduino.cc/en/Reference/PortManipulation
+
+There are multiple types of ports on an Arduino, to write to a port, you should use `digitalWrite` and `pinMode`. digitalWrite however has lots of overhead, so to optimize high frequency code, we can instead use port manipulation to directly access the registry where the data for the pins is stored. This process is called PortManipulation and is very powerfull and easy to do wrong. Doing something wrong here could destroy your board.
--- tone.ino
+++ tone.ino
@@ -7,32 +7,95 @@
 // Optimal samplerate for single sample processing, with a little headroom
 static const long sampleRate = 5000;
 
+const MidiNote notes[]
+{
+  { 24, 0.0625f, false },
+  { 0, 0.0625f, true },
+  { 36, 0.125f, false },
+  { 0, 0.25f, true },
+  { 24, 0.0625f, false },
+  { 0, 0.0625f, true },
+  { 39, 0.125f, false },
+  { 0, 1.125f, true },
+  { 60, 0.0625f, false },
+  { 0, 0.0625f, true },
+  { 36, 0.125f, false },
+  { 0, 0.25f, true },
+  { 24, 0.0625f, false },
+  { 0, 0.0625f, true },
+  { 36, 0.0625f, false },
+  { 35, 0.0625f, false },
+  { 0, 1, true }
+};
+const auto notesSize = sizeof(notes) / sizeof(MidiNote);
+
 constexpr float incrementFromFrequency(float frequency)
 {
   return 1.0f / (static_cast<float>(sampleRate) / frequency);
 }
 
+bool noteOff;
 float phaseIncrement = incrementFromFrequency(200.0f);
 float phase = 0.0f;
 inline bool processAudio()
 {
+  if(noteOff)
+  {
+    return false;
+  }
+    
   phase += phaseIncrement;
   phase -= floor(phase);
   
   return phase > 0.5f;
 }
 
+void loopNotes(int numTimes, float speedMultiplier)
+{
+  static const auto pin = 8;
+  
+  for(auto j = 0; j < numTimes; j++)
+  {
+    for (auto i = 0; i < notesSize; i++) 
+    {
+      const auto& note = notes[i];
+      const auto noteDuration = speedMultiplier * note.duration;
+        
+      if(note.rest)
+      {
+        noteOff = true;
+      }
+      else
+      {    
+        phaseIncrement = incrementFromFrequency(note.frequency);
+        noteOff = false;
+      }
+
+      delay(noteDuration);  
+    }
+  }
+}
+
 void TimerHandler(void)
 {
-  digitalWrite(8, processAudio() ? HIGH : LOW);
+  // Same as this, but using PortManipulation
+  //digitalWrite(8, processAudio() ? HIGH : LOW);
+
+  if(processAudio())
+  {
+    // Directly accesses the digital pins values
+    PORTB |= B00000001;
+  }
+  else
+  {
+    PORTB &= B11111110;
+  }
 }
 
 void setup() 
 {
   Serial.begin(9600);
   Serial.println("start");
-
-  constexpr float speedMultiplier =  multiplierFromBpm(120.0f);
 
   pinMode(8, OUTPUT);
 
@@ -41,6 +104,9 @@ void setup()
   {
     Serial.print(F("Starting  ITimer2 OK, millis() = ")); Serial.println(millis());
   }
+
+  constexpr float speedMultiplier =  multiplierFromBpm(60.0f);
+  loopNotes(100, speedMultiplier);
 }
 
 void loop() 
